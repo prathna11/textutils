ec599623c61d388af73769d8ae3a7da5604ca53a
prathna sri <prathna112001@gmail.com>
2024-06-15 23:23:40 +0530

    textutils files uploaded

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
-import logo from './logo.svg';
 import './App.css';
+import Nav from './component/Nav';
+import Alert from './component/Alert';
+import Textbox from './component/Textbox';
+import React, { useState} from 'react';
+//import AboutUs from './component/AboutUs';
+
+{/*import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Link
+} from "react-router-dom" */}
+
 
 function App() {
+  const [alert , setAlert]= useState(null);
+  const showAlert = ( message, type) =>{
+    setAlert({
+           msg: message ,
+           type: type
+    })
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+
+  }
+
+  const [mode , setMode] = useState('light');
+  let toggleMode = ()=>{
+    if(mode==='light'){
+      setMode('dark');
+      document.body.style.backgroundColor = "grey";
+      showAlert('enabled dark mode','success');
+    }
+  
+  else {
+    setMode('light');
+    document.body.style.backgroundColor = 'white';
+    showAlert('enabled light mode','success');
+
+  }
+}
+ let changetoColormode = () =>{
+  if(mode==='light'|| mode==='dark'){
+    setMode('pink')
+    document.body.style.backgroundColor = '#FFC0CB';
+    
+  }
+  else{
+    setMode('light')
+    document.body.style.backgroundColor = 'white';
+  }
+ }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <>
+    {/*<Router>*/}
+   <Nav title ='TextUtils' mode={mode} toggleMode={toggleMode} changetoColormode={changetoColormode}/>
+   <Alert alert={alert}/>
+     {/* <Routes>
+        <Route path='/about' element={<AboutUs />} />
+        <Route path='/' element={<Textbox mode={mode} showAlert={showAlert} />} />
+
+      </Routes>
     </div>
+
+    </Router> */}
+    <Textbox mode={mode} showAlert={showAlert}/>
+  
+   </>
   );
 }
 
---
+++ src/component/Textbox.js
@@ -0,0 +1,78 @@
+import React,{useState} from 'react'
+
+export default function Textbox(props) {
+ 
+ const handlecapitalalter = () =>{
+  let i = 0;
+  let newText = '';
+  for(i=0;i<=text.length;i++){
+      if(i%2===0){
+        newText += text.charAt(i).toUpperCase();
+      }
+      else{
+        newText += text.charAt(i).toLowerCase();
+
+      }
+      props.showAlert('The first letter is capitalizes','success');
+
+  }
+  setText(newText);
+
+ }
+
+  const handleclear = () =>{
+    let newText = ("")
+      setText(newText);
+      props.showAlert('The text is cleared','success');
+
+    
+  }
+  const handlecap= () => {    
+    let newtext = text.charAt(0).toUpperCase() + text.slice(1);
+    setText(newtext);
+    props.showAlert('The text is capitalized','success');
+
+  };
+  const handleupcase = ()=>{
+    let newText = text.toUpperCase();
+    setText(newText);
+    props.showAlert('converted to uppercase','danger');
+
+   }
+
+   const handlelowcase = ()=>{
+    let newText = text.toLowerCase();
+    setText(newText);
+    props.showAlert('converted to lowercase','primary');
+
+   }
+
+   const handleonchange = (event)=>{
+    setText(event.target.value)
+   }
+   const [text , setText] = useState('');
+
+  return (
+    <>
+        <div className="container my-3">
+                <h1 style= {{color:  props.mode==='light'?'black':'white'}}>WRITE ANY TEXT </h1>
+              <textarea className="form-control" style = {{backgroundColor: props.mode==='dark'?'grey':'white', color:  props.mode==='light'?'black':'white'}}onChange={handleonchange} value={text} id="exampleFormControlTextarea1" rows="8"></textarea>
+            <div> 
+              <button type="button" onClick={handleupcase} className="btn btn-dark my-3"  >Convert to upperclass</button>
+              <button type="button" onClick={handlelowcase} className="btn btn-dark my-3 mx-3" >Convert to lowerclass</button>
+              <button type='button' onClick={handlecap} className="btn btn-dark my-3 mx-1">capitalize</button>
+              <button type="button" onClick={handleclear} className="btn btn-dark my-3"  >clear</button>
+              <button type='button' onClick={handlecapitalalter} className="btn btn-dark my-3 mx-1">Alternate capital</button>
+
+
+            </div>
+            <div style= {{color:  props.mode==='light'?'black':'white'}}>
+             <h2 >your text summary</h2>
+             <p> {text.split(" ").length} words  and   {text.length} characters</p>
+             <h2>Preview</h2>
+             <p>{text}</p>
+             </div>
+            </div>
+   </>
+  )
+}
